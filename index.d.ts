c875cae7ea96baa70fbac13f7f98a624b9a85beb
phi0411141 <hongphipham97@gmail.com>
2025-01-08 18:02:19 +0700

    Fix interop-css

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export { tva } from 'packages/nativewind-utils/tva/index';
+export type { VariantProps } from 'packages/nativewind-utils/types';
---
+++ tva/index.d.ts
@@ -0,0 +1,2 @@
+import type { TVA } from 'packages/nativewind-utils/types';
+export declare const tva: TVA;
